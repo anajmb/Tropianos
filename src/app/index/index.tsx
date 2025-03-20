ec28422f07da3b86435a86a559c7e0d30abe7efe
Ana Julia Mathias <estrelaanamb0108@gmail.com>
2025-03-20 16:51:30 -0300

    Adicionando menu

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,15 +1,64 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, Touchable, TouchableOpacity, ScrollView, View } from "react-native";
 import { styles } from "./style"
 
 
 export default function Index() {
+const MENU = [
+    {
+        id: 1,
+        name: "Lasanha",
+        description: "Massa de lasanha caseira, queijo mussarela...",
+        price: 25.50,
+        image: require("@/assets/images/lasanha.jpg")
+    },
+    {
+        id: 2,
+        name: "Fettucine",
+        description: "Massa de fettucine caseira, queijo mussarela...",
+        price: 25.50,
+        image: require("@/assets/images/fettucine.jpg")
+    },
+    {
+        id: 3,
+        name: "Espaguete",
+        description: "Massa de espaguete caseira, queijo mussarela...",
+        price: 25.50,
+        image: require("@/assets/images/espaguete.jpeg")
+    }
+]
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Image style={styles.headerImage} source={require("../assets/images/header.png")} />
+                <Image style={styles.headerImage} source={require("@/assets/images/header.png")} />
                 <Text style={styles.restaurantName}>Tropiano's</Text>
+                <Text style={styles.cardapioName}>Cardápio</Text>
                 <Text style={styles.subTitle}>sua melhor experiência culinária</Text>
             </View>
+
+            <View style={styles.tabs}> 
+                {["Massas", "Acompanhamentos", "Sobremesas", "Bebidas"].map((item) => (
+                    <TouchableOpacity>
+                        <Text style={styles.tabsName}>{item}</Text>
+                    </TouchableOpacity>
+                ))}
+            </View>
+
+                <ScrollView style={styles.menuList}>
+
+            {
+                MENU.map((item) => (
+                    <TouchableOpacity style={styles.menuItem}>
+                    <View style={styles.menuContent}>
+                        <Text style={styles.itemName}>{item.name}</Text>
+                        <Text style={styles.itemDescription}>{item.description}</Text>
+                        <Text style={styles.itemPrice}>{item.price.toFixed(2)}</Text>
+                    </View>
+                    <Image style={styles.itemImage} source={item.image}></Image>
+                </TouchableOpacity>
+              ))
+            }
+            </ScrollView>
         </View>
     )
 }   
--- src/app/index/style.ts
+++ src/app/index/style.ts
@@ -11,18 +11,69 @@ export const styles = StyleSheet.create({
    headerImage: {
     width: "100%",
     height: 200,
-    borderRadius: 8
+    borderRadius: 8,
+    
    },
    restaurantName: {
     fontSize: 40,
-    fontWeight: "bold",
     position: "absolute",
     marginBlock: 80,
-    marginLeft: 100,
-    color: "#fff"
+    marginLeft: 110,
+    color: "#fff",
+    
+   },
+   cardapioName: {
+     fontWeight: 400,
+     textAlign: "center",
+     fontSize: 25,
+     marginTop: 15
    },
    subTitle: {
-    color: "#666",
-    marginTop: 4
+    fontWeight: 300,
+    textAlign: "center",
+    color: "#8e8e8e",
+    marginTop: 10
+   },
+   tabs: {
+      flexDirection: "row",
+      padding: 16,
+      borderBottomColor: "#eee",
+      borderBottomWidth: 1,
+      gap: 16
+   },
+   tabsName: {
+      color: "#666"
+   }, 
+   menuList: {
+      padding: 16,
+      flex: 1
+   },
+   menuItem: {
+       flexDirection: "row",
+      borderBottomColor: "#eee",
+      borderBottomWidth: 1,
+      margin: 10
+   }, 
+   menuContent: {
+      flex: 1,
+      marginRight: 16
+   },
+   itemImage: {
+      width: 80,
+      height: 80,
+      borderRadius: 8
+   },
+   itemName: {
+      fontSize: 16,
+      fontWeight: "bold"
+   },
+   itemDescription: {
+      color: "#666",
+      marginTop: 4
+   },
+   itemPrice: {
+      fontSize: 16,
+      fontWeight: "bold",
+      marginTop: 8
    }
 })
